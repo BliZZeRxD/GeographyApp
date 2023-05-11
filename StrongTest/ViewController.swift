e26eae7fcb2d4ebc5837afde5e55fb4e9a46008e
BliZZeRxD <blizzerxd@gmail.com>
2023-05-11 16:22:01 +0600

    Initial Commit

---
+++ StrongTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StrongTest
+//
+//  Created by Ayan on 11.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
